0b9f95df4c46b7bfdd3034e175e42ed2b6ddd7cb
kennbeckloff <kennbeckloff688@gmail.com>
2022-08-17 12:41:57 +0300

    commit tree 2ca0e16ede30f5ef6c5cf17bc6a86007dcb688c2

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+# The parrot method should accept an argument of a string and both puts out that string and return the string at the end of the method.
+# The parrot method should have a default argument of "Squawk!".
+ 
+def parrot(phrase="Squawk!")
+    puts phrase
+    return phrase
+end
